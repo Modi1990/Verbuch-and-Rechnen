ad6e06a7cef6ebb7f6b841e20791bde62b52536a
Modi1990 <Mohanned.yahya@js-soft.com>
2020-05-02 16:02:16 +0200

    Add new Output Message!

--- Verbruch_Rechnen/src/Datentype01.java
+++ Verbruch_Rechnen/src/Datentype01.java
@@ -3,21 +3,23 @@ public class Datentype01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		byte a;
-		byte num=127;
-		byte y=5;
+		byte num = 127;
+		byte y = 5;
 		byte ergeb;
-		a =(byte) 256;
+		a = (byte) 256;
 		System.out.println("Das Ergebnis in Byte für (256) = " + a);
-		a =  (byte) 128;
+		a = (byte) 128;
 		System.out.println("Das Ergebnis in Byte für (128) = " + a);
-		a =  (byte) 255;
+		a = (byte) 255;
 		System.out.println("Das Ergebnis in Byte für (255) = " + a);
-		a =  (byte) 200;
+		a = (byte) 200;
 		System.out.println("Das Ergebnis in Byte für (255) = " + a);
 		ergeb = (byte) (num + y);
 		System.out.println("Das Ergebnis in Byte für (Ergeb) = " + ergeb);
+
+		System.out.println("The program is completed");
 
 	}
 
